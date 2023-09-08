468e1911ae1604214a6a204d2b6d196c4e359200
Jerry Yant <jyantaratan@cps.edu>
2023-09-08 11:11:05 -0500

    y

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -6,11 +6,31 @@ computer_choice = "paper"
 
 def get_choices():
     options = ['rock', 'paper', 'scissors']
-    player_choice = "rock"
+    player_choice = input("Enter a choice (rock, paper, scissors): ")
     computer_choice = random.choice(options)
     choices = {"player": player_choice, "computer": computer_choice}
 
     return choices
 
-choices = get_choices()
-print(choices)
+def check_winner (player, computer):
+    print(f"you chose {player}, computer chose {computer}")
+    if player == computer:
+        return "it's a tie"
+    elif player == "rock":
+        if computer == "scissors":
+            return "rock smashes scissors, You win!"
+        else:
+            return "paper covers rock, You lose!"
+    elif player == "paper":
+        if computer == "paper":
+            return "paper covers rock, You win!"
+        else:
+            return "scissors cuts paper, You lose!"
+    elif player == "scissors":
+        if computer == "scissors":
+            return "scissors cuts paper, You win!"
+        else:
+            return "rock smashes scissors, You lose!"
+    
+result = check_winner("rock","rock")
+print(result)
